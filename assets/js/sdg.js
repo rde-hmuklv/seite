1d67f7ff96501bad9c9f1c8cf38250b560bcaea8
rde-hmuklv <128131718+rde-hmuklv@users.noreply.github.com>
2023-11-27 10:29:39 +0000

    Deploying to gh-pages from  @ f050e2953fbd6910a5bd8d80482b9f0856d96fa7 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -5053,7 +5053,7 @@ var indicatorInit = function () {
                     var view = new indicatorView(model, {
                         rootElement: '#indicatorData',
                         legendElement: '#plotLegend',
-                        decimalSeparator: '',
+                        decimalSeparator: ',',
                         maxChartHeight: 420,
                         tableColumnDefs: [
                             { maxCharCount: 25 }, // nowrap
